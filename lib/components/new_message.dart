1b6a2cdb3ce55e39de9abdb7e2b27cfc24f5dc22
Rafaella Barbosa <rafaellacsta@hotmail.com>
2025-08-29 17:22:58 -0300

    feat(chat): envio de mensagens co m NewMessage

---
+++ lib/components/new_message.dart
@@ -0,0 +1,51 @@
+import 'package:chat/core/services/auth/auth_service.dart';
+import 'package:chat/core/services/chat/chat_service.dart';
+import 'package:flutter/material.dart';
+
+class NewMessage extends StatefulWidget {
+  const NewMessage({super.key});
+
+  @override
+  State<NewMessage> createState() => _NewMessageState();
+}
+
+class _NewMessageState extends State<NewMessage> {
+  String _message = '';
+  final _messageController = TextEditingController();
+
+  Future<void> _sendMessage() async {
+    final user = AuthService().currentUser;
+
+    if (user != null) {
+      final msg = await ChatService().save(_message, user);
+      print(msg?.id);
+      _messageController.clear();
+      setState(() => _message = '');
+    }
+  }
+
+  @override
+  Widget build(BuildContext context) {
+    return Row(
+      children: [
+        Expanded(
+          child: TextField(
+            controller: _messageController,
+            textInputAction: TextInputAction.send,
+            onChanged: (msg) => setState(() => _message = msg),
+            decoration: InputDecoration(labelText: 'Enviar mensagem...'),
+            onSubmitted: (_) {
+              if (_message.trim().isNotEmpty) {
+                _sendMessage();
+              }
+            },
+          ),
+        ),
+        IconButton(
+          icon: Icon(Icons.send),
+          onPressed: _message.trim().isEmpty ? null : _sendMessage,
+        ),
+      ],
+    );
+  }
+}
